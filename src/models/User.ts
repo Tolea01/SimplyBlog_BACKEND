5ce782da30b195b9f33a47324115a873da47efc1
Tolea01 <anatoliecurbat@gmail.com>
2023-06-13 02:27:38 +0300

    config database & UserModel

---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Model } from "mongoose";
+
+interface User extends Document {
+  fullName: string,
+  email: string,
+  passwordHash: string,
+  avatarUrl?: string
+}
+
+const UserSchema = new mongoose.Schema<User>(
+  {
+    fullName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      require: true
+    },
+    passwordHash: {
+      type: String,
+      require: true
+    },
+    avatarUrl: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+const UserModel: Model<User> = mongoose.model<User>('User', UserSchema)
+
+export default UserModel;
