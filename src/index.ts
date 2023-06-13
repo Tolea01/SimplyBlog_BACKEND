ca5ae949cb94ec8bfb3e8f00b355655f45776f6b
Tolea01 <anatoliecurbat@gmail.com>
2023-06-14 02:58:14 +0300

    login & register

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,104 @@
 import express, { Express, Response, Request } from 'express';
-import jwt, { Secret } from "jsonwebtoken";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth';
+import { validationResult, Result } from 'express-validator/src/validation-result';
+import UserModel from './models/User';
 
-mongoose.connect('mongodb+srv://pcuser876:admin@cluster0.laynzuo.mongodb.net/?retryWrites=true&w=majority')
-.then(() => console.log('DB OK'))
-.catch(err => console.log('DB EROOR', err));
+mongoose
+  .connect('mongodb+srv://pcuser876:admin@cluster0.laynzuo.mongodb.net/blog?retryWrites=true&w=majority')
+  .then(() => console.log('DB OK'))
+  .catch(err => console.log('DB EROOR', err));
 
 const app: Express = express();
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+const generateToken = (userId: string): string => {
+  return jwt.sign(
+    {
+      _id: userId,
+    },
+    'secret-123',
+    {
+      expiresIn: '30d'
+    }
 
-app.post('/login', (req: Request, res: Response) => {
-  interface Payload {
-    email: string,
-    fullName: string
+  );
+}
+
+const handleErrors = async (err: any, res: Response, status: number, message: string) => {
+  console.log(err);
+  res.status(status).json({ message })
+};
+
+app.post('/login', async (req: Request, res: Response) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'You are not registered' });
+    }
+
+    const isValidPass: boolean = await bcrypt.compare(req.body.password, user.toObject().passwordHash);
+
+    if (!isValidPass) {
+      return res.status(400).json({ message: 'Invalid login or password' });
+    }
+
+    const token: string = generateToken(user._id);
+
+    res.json({
+      ...user.toObject(),
+      token
+    });
+
+  } catch (err) {
+    return handleErrors(err, res, 400, "Failed to login");
   }
-
-  const secret: Secret = 'secret-key';
-
-  const token: string = jwt.sign({
-    email: req.body.email,
-    fullName: 'test'
-  } as Payload, secret);
-
-  res.json({
-    succes: true,
-    token
-  });
 })
 
-app.listen(4444, () => console.log('Server OK!'));;
+app.post('/register', registerValidation, async (req: Request, res: Response) => {
+  try {
+    const errors: Result = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors)
+    }
+
+    const password: string = req.body.password;
+    const salt: string = await bcrypt.genSalt(10);
+    const passwordHash: string = await bcrypt.hash(password, salt);
+
+    const document = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await document.save();
+
+    const token: string = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret-123',
+      {
+        expiresIn: '30d'
+      }
+
+    )
+
+    res.json({
+      ...user.toObject(),
+      token
+    });
+
+  } catch (err) {
+    return handleErrors(err, res, 500, "Error, registration could not be performed");
+  }
+})
+
+app.listen(4444, () => console.log('Server OK!'));
